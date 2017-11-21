e8e520d8cbe8319015f2b279828008d158cadb94
Harshad <harshadce2008@gmail.com>
2017-11-21 10:34:24 +0530

    add production file changes

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -76,4 +76,23 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  config.action_mailer.asset_host = "https://hidden-escarpment-22538.herokuapp.com/"
+
+  config.action_mailer.default_url_options = { host: 'https://hidden-escarpment-22538.herokuapp.com/', only_path: false }
+
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.default :charset => "utf-8"
+
+  config.action_mailer.smtp_settings = {
+      :address              => "smtp.gmail.com",
+      :port                 => 587,
+      :domain               => "gmail.com",
+      :user_name            => "itserviceyouth@gmail.com",
+      :password             => "B@dashah26",
+      :authentication       => :plain,
+      :enable_starttls_auto => true
+  }
 end
